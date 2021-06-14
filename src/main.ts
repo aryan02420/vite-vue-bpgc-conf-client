cb98eab55b25b427cf3cf82dcc684af41a139b95
aryan02420 <aryan02420@gmail.com>
2021-06-14 14:51:54 +0530

    add auth0

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import { store } from './store'
-import './index.css'
-
-createApp(App)
-    .use(store)
-    .mount('#app')
+import router from '@/router'
+import App from '@/App.vue'
+import { store } from '@/store'
+import '@/index.css'
+import { Auth0 } from '@/auth'
+import { domain, clientId, audience } from '@/auth_config.json'
+;(async () => {
+  const AuthPlugin = await Auth0.init({
+    clientId,
+    domain,
+    audience,
+    useRefrestTokens: true,
+    redirectUri: window.location.origin,
+    onRedirectCallback: (appState: any) => {
+      router.push(appState?.targetUrl ?? window.location.pathname)
+    },
+  })
+  const app = createApp(App)
+  app.use(router)
+  app.use(store)
+  app.use(AuthPlugin)
+  app.mount('#app')
+})()
