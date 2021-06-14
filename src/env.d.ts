632a4b01db3be637ded34560f667d89658709147
aryan02420 <aryan02420@gmail.com>
2021-06-14 16:30:22 +0530

    add auth env variables

---
+++ src/env.d.ts
@@ -0,0 +1,5 @@
+interface ImportMetaEnv {
+  VITE_AUTH_DOMAIN: string
+  VITE_AUTH_CLIENTID: string
+  VITE_AUTH_AUDIENCE: string
+}
--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,11 @@ import App from '@/App.vue'
 import { store } from '@/store'
 import '@/index.css'
 import { Auth0 } from '@/auth'
-import { domain, clientId, audience } from '@/auth_config.json'
 ;(async () => {
   const AuthPlugin = await Auth0.init({
-    clientId,
-    domain,
-    audience,
+    clientId: import.meta.env.VITE_AUTH_CLIENTID,
+    domain: import.meta.env.VITE_AUTH_DOMAIN,
+    audience: import.meta.env.VITE_AUTH_AUDIENCE,
     useRefrestTokens: true,
     redirectUri: window.location.origin,
     onRedirectCallback: (appState: any) => {
