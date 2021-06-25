7582c4161c4ef938229478fd2c2a9316a7efd555
aryan02420 <aryan02420@gmail.com>
2021-06-26 04:46:00 +0530

    auto hide on scroll

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <TopBar class="fixed left-0 right-0 top-0 z-10" />
+  <TopBar
+    class="fixed left-0 right-0 top-0 z-10 duration-500"
+    :style="{
+      top: `${this.scrollNormPos > 0 ? -1 * this.scrollNormPos : 0}px`,
+    }"
+  />
   <router-view v-slot="{ Component }">
     <transition name="fade-down" mode="out-in">
       <keep-alive>
@@ -7,14 +12,20 @@
       </keep-alive>
     </transition>
   </router-view>
-  <BottomNav class="fixed left-1/2 transform -translate-x-1/2 bottom-2 z-10" />
+  <BottomNav
+    class="fixed left-1/2 transform -translate-x-1/2 mb-2 bottom-0 z-10 duration-500"
+    :style="{
+      bottom: `${this.scrollNormPos > 0 ? -1 * this.scrollNormPos : 0}px`,
+    }"
+  />
 </template>
 
 <script lang="ts">
-import { defineComponent, inject } from 'vue'
+import { defineComponent, inject, ref } from 'vue'
 import { useStore } from 'vuex'
 import BottomNav from '@/components/bottomNav.vue'
 import TopBar from '@/components/topBar.vue'
+import { throttle } from 'throttle-debounce'
 
 export default defineComponent({
   name: 'App',
@@ -22,6 +33,17 @@ export default defineComponent({
     BottomNav,
     TopBar,
   },
+  // data() {
+  //   return {
+  //     scrollPos: {
+  //       curr: 0,
+  //       prev: 0,
+  //       pivot: 0,
+  //       dir: 1,
+  //       norm: 0,
+  //     },
+  //   }
+  // },
   methods: {
     async getToken() {
       if (!this.Auth.isAuthenticated.value) return
@@ -29,16 +51,58 @@ export default defineComponent({
       token = JSON.parse(window.atob(token.split('.')[1]))
       console.log(token)
     },
+    // _onscroll: function () {
+    //   console.log(Date.now())
+    //   let curr = window.scrollY
+    //   let prev = this.scrollPos.curr
+    //   let dir = Math.sign(curr - prev)
+    //   if (dir !== this.scrollPos.dir) {
+    //     this.scrollPos.pivot = curr
+    //   }
+    //   this.scrollPos.norm = curr - this.scrollPos.pivot
+    //   this.scrollPos.prev = this.scrollPos.curr
+    //   this.scrollPos.curr = curr
+    //   this.scrollPos.dir = dir
+    // },
+  },
+  created() {
+    document.addEventListener('scroll', this.onScroll)
+  },
+  destroyed() {
+    document.removeEventListener('scroll', this.onScroll)
   },
   setup() {
     const store = useStore()
     console.log(store)
     const Auth: any = inject('Auth')
+    const scrollPos = {
+      curr: 0,
+      prev: 0,
+      pivot: 0,
+      dir: 1,
+    }
+    let scrollNormPos = ref(0)
+    const _onscroll = function () {
+      let curr = window.scrollY
+      let prev = scrollPos.curr
+      let dir = Math.sign(curr - prev)
+      if (dir !== scrollPos.dir) {
+        scrollPos.pivot = curr
+      }
+      scrollNormPos.value = curr - scrollPos.pivot
+      scrollPos.prev = scrollPos.curr
+      scrollPos.curr = curr
+      scrollPos.dir = dir
+    }
+    const onScroll = throttle(200, true, _onscroll)
     return {
       Auth,
       store,
+      onScroll,
+      scrollNormPos,
     }
   },
+  computed: {},
 })
 </script>
 
